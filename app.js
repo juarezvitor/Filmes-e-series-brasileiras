e396a35cdb3188c731e4db1a984fb9debe66186a
Juarez Vitor <160255906+juarezvitor@users.noreply.github.com>
2024-09-08 14:05:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+function pesquisar() {
+    // Seleciona a seção HTML onde os resultados da pesquisa serão exibidos.
+    // O ID "resultados-pesquisa" é usado como referência para encontrar o elemento.
+    let section = document.getElementById("resultados-pesquisa");
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(campoPesquisa == "") {
+      section.innerHTML = "<p>Digite um nome de filme ou série<p>"
+      return}
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    // Inicializa uma string vazia para armazenar o HTML dos resultados.
+    // Essa string será preenchida dinamicamente a cada iteração do loop.
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada item (dado) na base de dados (dados.js).
+    // Para cada item, um novo elemento HTML será criado e adicionado à string 'resultados'.
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      //se titulo ou descricao estiver incluido 
+      if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="${dado.linkcritica}" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta"><strong>${dado.titulo}</strong>${dado.descricao}.</p>
+          <a href="${dado.link}" target="_blank"><strong>Onde assistir</strong></a>
+        </div>
+      `;
+      }
+     if(!resultados){
+      resultados = "<p>Filme ou série não foi encontrado<p>"
+     } 
+    } 
+    // Substitui o conteúdo HTML da seção "resultados-pesquisa" pelo conteúdo gerado.
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [    
+    {
+        titulo : "Pedro Dom",
+        descricao : " é uma série brasileira que mergulha na complexa relação entre um pai e um filho, ambos marcados pelo mundo do crime. A trama se baseia em fatos reais e acompanha a vida de Pedro Dom, um jovem que se torna um dos maiores criminosos do Rio de Janeiro, e de seu pai, Victor Lomba, um policial que dedica sua vida à luta contra as drogas",
+        link : "https://www.primevideo.com/-/pt/detail/DOM/0RXMEPPBWG40PBHLDFG8E8QJKN",
+        linkcritica : "https://www.omelete.com.br/series-tv/criticas/dom-3-temporada-critica",
+        tags: "policia policial playboy "
+    },
+    {
+        titulo : "Cangaço Novo",
+        descricao : " é uma série brasileira que reinventa o clássico mito do cangaço, transportando-o para os dias atuais. A trama acompanha Ubaldo, um bancário insatisfeito com sua vida em São Paulo, que descobre ser herdeiro de uma família do sertão cearense com um passado ligado ao famoso movimento de bandidos.",
+        link : "https://www.primevideo.com/dp/amzn1.dv.gti.a00fdab6-1391-4871-a0b2-5be765193506?autoplay=0&ref_=atv_cf_strg_wb",
+        linkcritica : "https://www.omelete.com.br/series-tv/criticas/cangaco-novo-1a-temporada",
+        tags: "policia"
+    },
+    {
+        titulo: "Largados e Pelados Brasil",
+        descricao: " Dez brasileiros enfrentam o desafio de sobreviver por 21 dias na selva amazônica, sem comida, água ou abrigo. A série acompanha a jornada desses participantes enquanto lutam contra a natureza selvagem e seus próprios limites.",
+        link: "https://www.max.com/br/pt/shows/largados-e-pelados-brasil-a-reuniao/0666f06b-6d69-4e7c-aefa-9b03892d8f9b?utm_source=universal_search",
+        linkcritica: "",
+        tags: "sobrevivencia mato floresta desafio brasil"
+      },
+      {
+        titulo: "Sob Pressão",
+        descricao: " Uma série dramática que acompanha o dia a dia de uma equipe médica de um hospital público no Rio de Janeiro, mostrando os desafios e as emoções de salvar vidas em um sistema de saúde sobrecarregado.",
+        link: "https://globoplay.globo.com/sob-pressao/t/PhQ7Wtzn6C/?origemId=91698",
+        linkcritica: "https://www.adorocinema.com/filmes/filme-250199/criticas-adorocinema/",
+        tags: "carioca "
+      },
+      {
+        titulo: "Arcanjo Renegado",
+        descricao: " Uma série de ação que acompanha a história de Miguel, um ex-militar que se torna um justiceiro, combatendo a corrupção e o crime organizado.",
+        link: "https://globoplay.globo.com/arcanjo-renegado/t/jqvngb9dQG/?origemId=91698",
+        linkcritica: "https://www.oxentepipoca.com.br/post/cr%C3%ADtica-arcanjo-renegado-2%C2%AA-temporada",
+        tags:"militar corrupto"
+      },
+];
+
