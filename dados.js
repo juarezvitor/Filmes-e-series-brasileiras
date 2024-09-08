5e8126a3697888c5da7a1b19654b36337a5dcd8f
Juarez Vitor <160255906+juarezvitor@users.noreply.github.com>
2024-09-08 15:47:19 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -39,119 +39,112 @@ let dados = [
         descricao: "Uma série distópica que acompanha um grupo de jovens que participam de um jogo cruel para garantir um futuro melhor.",
         link: "https://www.netflix.com/br/title/80112114",
         linkcritica: "https://www.adorocinema.com/filmes/filme-255581/",
-        tags: "distopia, futuro, suspense"
+        tags: "distopia futuro suspense"
       },
       {
         titulo: "Cidade de Deus",
         descricao: "Um filme que retrata a vida em uma favela carioca, mostrando a violência, o tráfico de drogas e a luta pela sobrevivência.",
         link: "https://www.adorocinema.com/filmes/filme-34889/",
         linkcritica: "https://www.imdb.com/title/tt0317248/",
-        tags: "favela, crime, drama"
+        tags: "favela crime drama"
       },
       {
-        titulo: "Bacurau",
-        descricao: "Um filme de ficção científica que explora temas como violência, xenofobia e a luta por sobrevivência em um futuro próximo.",
-        link: "https://www.adorocinema.com/filmes/filme-267424/",
-        linkcritica: "https://www.empireonline.com/movies/reviews/bacurau/",
-        tags: "ficção científica, violência, futuro"
-      },
-      {
-        titulo: "O Esquadrão",
-        descricao: "Uma série policial que acompanha uma equipe de elite da polícia militar que luta contra o crime organizado no Rio de Janeiro.",
-        link: "https://globoplay.globo.com/o-esquadrao/t/jqvngb9dQG/?origemId=91698",
-        linkcritica: "https://www.ofuxico.com.br/noticias/tv/o-esquadrao-serie-da-globoplay-ganha-trailer-e-data-de-estreia--20230120174149.html",
-        tags: "policial, ação, crime"
+        titulo: "Tropa de elite 2",
+        descricao: "O filme Tropa de Elite 2: O Inimigo Agora É Outro (2010) conta a história de Nascimento (Wagner Moura), um ex-capitão do BOPE que é afastado da corporação após uma operação fracassada. Ele passa a trabalhar na Secretaria de Segurança Pública do Rio de Janeiro como subsecretário de Inteligência e descobre que o sistema é mais podre do que imaginava.",
+        link: "https://globoplay.globo.com/tropa-de-elite-2-o-inimigo-agora-e-outro/t/x9qXTWGxTH/?origemId=91698",
+        linkcritica: "https://pt.wikipedia.org/wiki/Tropa_de_Elite_2:_O_Inimigo_Agora_%C3%89_Outro",
+        tags: "policial ação crime"
       },
       {
         titulo: "As Boas Maneiras",
         descricao: "Um filme de terror que mistura elementos sobrenaturais com a realidade social brasileira, abordando temas como racismo e desigualdade social.",
-        link: "https://www.adorocinema.com/filmes/filme-255581/",
-        linkcritica: "https://www.timeout.com/rio-de-janeiro/filme/as-boas-maneiras",
-        tags: "terror, sobrenatural, social"
-      },
-      {
-        titulo: "A Divisão",
-        descricao: "Uma série policial que acompanha o trabalho de uma equipe especializada em crimes de alta complexidade.",
-        link: "https://globoplay.globo.com/a-divisao/t/jqvngb9dQG/?origemId=91698",
-        linkcritica: "https://www.ofuxico.com.br/noticias/tv/a-divisao-serie-da-globoplay-ganha-trailer-e-data-de-estreia--20230120174149.html",
-        tags: "policial, suspense, investigação"
+        link: "https://www.netflix.com/br/title/81007860?source=35",
+        linkcritica: "https://www.adorocinema.com/filmes/filme-214098/",
+        tags: "terror sobrenatural social"
       },
       {
         titulo: "O Mecanismo",
         descricao: "Uma série que explora o universo da corrupção política no Brasil, inspirada em fatos reais.",
-        link: "https://www.netflix.com/br/title/80112114",
-        linkcritica: "https://www.adorocinema.com/filmes/filme-255581/",
-        tags: "corrupção, política, suspense"
+        link: "https://www.netflix.com/br/title/80120485?source=35",
+        linkcritica: "https://pt.wikipedia.org/wiki/O_Mecanismo",
+        tags: "corrupção política suspense"
       },
       {
         titulo: "A Mata Negra",
         descricao: "Um filme de terror que se passa em uma pequena cidade e explora temas como lendas urbanas e possessão demoníaca.",
-        link: "https://www.adorocinema.com/filmes/filme-267424/",
-        linkcritica: "https://www.empireonline.com/movies/reviews/bacurau/",
-        tags: "terror, sobrenatural, lendas"
+        link: "https://www.primevideo.com/-/pt/detail/A-Mata-Negra/0NLQGENPW5Q5T5QUTR79UNVOZD",
+        linkcritica: "https://www.adorocinema.com/filmes/filme-264400/",
+        tags: "terror sobrenatural lendas"
+      },
+      {
+        titulo: "A Divisão",
+        descricao: "Uma série policial que acompanha o trabalho de uma equipe especializada em crimes de alta complexidade.",
+        link: "https://globoplay.globo.com/a-divisao/t/wDn8QHHyp2/?origemId=91698",
+        linkcritica: "https://pt.wikipedia.org/wiki/A_Divis%C3%A3o",
+        tags: "policial suspense investigação"
       },
       {
         titulo: "Central do Brasil",
         descricao: "Uma história emocionante sobre uma senhora que escreve cartas para pessoas analfabetas e encontra uma criança perdida no caminho.",
-        link: "https://www.adorocinema.com/filmes/filme-11710/",
-        linkcritica: "https://www.imdb.com/title/tt0118799/",
-        tags: "drama, social, humanismo"
+        link: "https://www.netflix.com/br/title/8177424?source=35",
+        linkcritica: "https://pt.wikipedia.org/wiki/Central_do_Brasil_(filme)",
+        tags: "drama social humanismo"
       },
       {
           titulo: "O Que é Isso, Companheiro?",
           descricao: "Um documentário que retrata a luta armada no Brasil durante a ditadura militar.",
-          link: "https://www.adorocinema.com/filmes/filme-14542/",
-          linkcritica: "https://www.imdb.com/title/tt0108090/",
-          tags: "documentário, história, política"
+          link: "https://globoplay.globo.com/o-que-e-isso-companheiro/t/7TXqxPchRv/?origemId=91698",
+          linkcritica: "https://pt.wikipedia.org/wiki/O_Que_%C3%89_Isso,_Companheiro%3F_(filme)",
+          tags: "documentário história política"
       },
       {
           titulo: "O Auto da Compadecida",
           descricao: "Uma adaptação cinematográfica da peça de Ariano Suassuna, que conta a história de João Grilo e Chicó em um mundo fantástico e cheio de humor.",
-          link: "https://www.adorocinema.com/filmes/filme-24070/",
-          linkcritica: "https://www.imdb.com/title/tt0332452/",
-          tags: "comédia, fantasia, regional"
+          link: "https://globoplay.globo.com/o-auto-da-compadecida/t/TMFdjjCFHM/?origemId=91698",
+          linkcritica: "https://pt.wikipedia.org/wiki/O_Auto_da_Compadecida_(filme)",
+          tags: "comédia fantasia regional"
       },
       {
           titulo: "Que Horas Ela Volta?",
           descricao: "Um drama social que aborda a questão das empregadas domésticas no Brasil, mostrando as relações entre patrões e empregadas e as dificuldades da vida na periferia.",
-          link: "https://www.adorocinema.com/filmes/filme-250143/",
-          linkcritica: "https://www.imdb.com/title/tt4132732/",
-          tags: "drama, social, desigualdade"
+          link: "https://www.netflix.com/br/title/80039527?source=35",
+          linkcritica: "https://pt.wikipedia.org/wiki/Que_Horas_Ela_Volta%3F",
+          tags: "drama social desigualdade"
       },
       {
           titulo: "Tropa de Elite",
           descricao: "Um filme que retrata a violência policial no Rio de Janeiro e as dificuldades de combater o crime organizado.",
-          link: "https://www.adorocinema.com/filmes/filme-34890/",
-          linkcritica: "https://www.imdb.com/title/tt0450259/",
-          tags: "ação, policial, violência"
+          link: "https://globoplay.globo.com/tropa-de-elite/t/hyrZ9WpsBw/?origemId=91698",
+          linkcritica: "https://pt.wikipedia.org/wiki/Tropa_de_Elite_(filme)",
+          tags: "ação policial violência"
       },
       {
           titulo: "O Cheiro do Ralo",
           descricao: "Um drama psicológico que acompanha a história de um jovem que retorna para sua cidade natal e se envolve em uma rede de crimes e mistérios.",
-          link: "https://www.adorocinema.com/filmes/filme-255644/",
-          linkcritica: "https://www.imdb.com/title/tt4323710/",
-          tags: "suspense, drama, psicológico"
+          link: "https://globoplay.globo.com/o-cheiro-do-ralo/t/wwMQw5RPGj/?origemId=91698",
+          linkcritica: "https://pt.wikipedia.org/wiki/O_Cheiro_do_Ralo_(filme)",
+          tags: "suspense drama psicológico"
       },
       {
           titulo: "Bacurau",
           descricao: "Um filme de ficção científica que explora temas como violência, xenofobia e a luta por sobrevivência em um futuro próximo.",
-          link: "https://www.adorocinema.com/filmes/filme-267424/",
-          linkcritica: "https://www.empireonline.com/movies/reviews/bacurau/",
-          tags: "ficção científica, violência, futuro"
+          link: "https://globoplay.globo.com/bacurau/t/R6ymtCdFmc/?origemId=91698",
+          linkcritica: "https://pt.wikipedia.org/wiki/Bacurau_(filme)",
+          tags: "ficção científica violência futuro"
       },
       {
           titulo: "O Tempo e o Vento",
           descricao: "Uma trilogia épica que retrata a história do Rio Grande do Sul desde o século XIX até os dias atuais.",
-          link: "https://www.adorocinema.com/filmes/filme-34888/",
-          linkcritica: "https://www.imdb.com/title/tt0410142/",
-          tags: "época, histórico, drama"
+          link: "https://www.netflix.com/br/title/81347805?source=35",
+          linkcritica: "https://pt.wikipedia.org/wiki/O_Tempo_e_o_Vento",
+          tags: "época histórico drama"
       },
       {
           titulo: "Aquarius",
           descricao: "Um drama que retrata a luta de uma arquiteta para manter seu apartamento em um prédio que será demolido para a construção de um novo empreendimento.",
-          link: "https://www.adorocinema.com/filmes/filme-250142/",
-          linkcritica: "https://www.imdb.com/title/tt4571114/",
-          tags: "drama, social, resistência"
+          link: "https://www.netflix.com/br/title/80113667?source=35",
+          linkcritica: "https://pt.wikipedia.org/wiki/Aquarius_(filme)",
+          tags: "drama social resistência"
       }
 ];
 
